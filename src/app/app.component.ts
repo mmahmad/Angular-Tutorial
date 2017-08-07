2c20bc49aa98e5c24efd0e9d92040bbcc9f8ca94
mmahmad <mohammad.moaaz.ahmad@gmail.com>
2017-08-07 17:19:53 +0500

    Add master/detail; use *ngIf to show details only when hero selected; customize color of selected hero.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,21 @@ import { Component } from '@angular/core';
 
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
       <!-- each hero goes here -->
       <span class="badge">{{hero.id}}</span>{{hero.name}}
     </li>
   </ul>
 
   <h2>
-    {{hero.name}} details!
+    {{selectedHero.name}} details!
   </h2>
 
-  <div><label>id: </label>{{hero.id}}</div>
+  <div *ngIf="selectedHero">
+  <div><label>id: </label>{{selectedHero.id}}</div>
   <div><label>name: </label>
-  <input [(ngModel)]="hero.name" placeholder="name">
+  <input [(ngModel)]="selectedHero.name" placeholder="name">
+  </div>
   </div>
 
   `,
@@ -84,17 +86,13 @@ export class AppComponent {
   title = 'Tour of Heroes';
   // hero = 'Windstorm';
 
-  /**
-   * 
-   * 
-   * @type {Hero}
-   * @memberof AppComponent
-   * 
-   */
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+ 
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  selectedHero : Hero;
 
   private HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -111,8 +109,14 @@ export class AppComponent {
 
 heroes = this.HEROES;
 
+onSelect(hero : Hero) : void{
+  this.selectedHero = hero;
+}
+
 
 }
+
+
 
 export class Hero {
   id: number;
